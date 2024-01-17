1b430fefb0b227fef024885d1137a529512ed211
WaterIsNecessary <152506344+WaterIsNecessary@users.noreply.github.com>
2024-01-17 21:18:24 +0100

    Add function for generating computer response

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 let userInput = prompt(`Please enter your choice. Rock, Paper or Scissors? `);
+
+function getComputerChoice {
+    let rand = Math.floor(Math.random()*3);
+    let computerResponse;
+    if (rand === 0) {
+        computerResponse = "Rock";
+    } else if (rand === 1) {
+        computerResponse = "Paper";
+    } else {
+        computerResponse = "Scissors";
+    }
+    return computerResponse;
+}
